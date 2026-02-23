f1bf616d841ae30d7908c9b506b87b928ca2faea
Roman <romanvolovoy@gmail.com>
2026-02-23 14:09:30 +0100

    fix footer

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -2,8 +2,8 @@ import { ExternalLink } from "../utils/ExternalLink";
 
 export function Footer() {
   return (
-    <footer className="about-footer">
-      <p className="about-footer__text">
+    <footer className="footer">
+      <p className="footer__text">
         Created by{" "}
         <ExternalLink href="https://volovoy.com">Roman Volovoy</ExternalLink> •{" "}
         <ExternalLink href="https://github.com/romanzy313/fpps">
